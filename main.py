199d0c7b405df570aa7cb1f70cb82e7e5ba6d19c
TheeAuthor <mbarri0@outlook.com>
2025-07-23 17:39:41 -0400

    Draw player

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame, constants 
+import pygame, constants, player 
 
 def main():
     print("Starting Asteroids!")
@@ -13,18 +13,23 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    player1 = player.Player(x = constants.SCREEN_WIDTH / 2, y = constants.SCREEN_HEIGHT / 2 )
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill(color="black")
+        
+        player1.draw(screen)
+        
         pygame.display.flip()
         
         # Pause the game loop until 1/60th of a second has passed.
         clock.tick(60)
         dt = clock.tick() / 1000
 
-    
+        
 if __name__ == "__main__":
     main()
