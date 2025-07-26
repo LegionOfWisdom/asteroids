fc00e7f07f3de7d55242b158e9028d1513408e97
TheeAuthor <mbarri0@outlook.com>
2025-07-26 09:36:57 -0400

    added groups

--- main.py
+++ main.py
@@ -9,19 +9,28 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable,drawable)
+    
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2 )
+    
     dt = 0
-    
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
         
         screen.fill(color="black")
-        player.draw(screen)
+        #player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         
         # Pause the game loop until 1/60th of a second has passed.
